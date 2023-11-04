f4ac1348d7918f65562620e5c8605ee6415b2396
Duy Do <duydo@protonmail.com>
2023-11-04 12:21:53 +0700

    .

--- crates/cmds-std/src/lib.rs
+++ crates/cmds-std/src/lib.rs
@@ -1,14 +1,3 @@
-#[macro_export]
-macro_rules! node_definition {
-    ($file:expr $(,)?) => {
-        include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/node-definitions/",
-            $file
-        ))
-    };
-}
-
 pub mod const_cmd;
 pub mod json_extract;
 pub mod json_insert;
