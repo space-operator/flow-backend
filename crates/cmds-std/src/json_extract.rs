eb343e06459abf1f8676c608d2923f13cc79a6f2
Duy Do <duydo@protonmail.com>
2023-11-22 15:46:33 +0700

    .

--- crates/cmds-std/src/json_extract.rs
+++ crates/cmds-std/src/json_extract.rs
@@ -1,65 +1,29 @@
-use flow_lib::command::prelude::*;
+use flow_lib::{command::prelude::*, UserId};
 
-const JSON_EXTRACT: &str = "json_extract";
-
-#[derive(Deserialize, Debug)]
-struct Input {
-    json_input: Value,
-    field_path: String,
-}
-
-#[derive(Serialize, Debug)]
-struct Output {
-    value: Value,
-    trimmed_json: Value,
-}
-
-async fn run(_: Context, mut input: Input) -> Result<Output, CommandError> {
-    let path = value::crud::path::Path::parse(&input.field_path)?;
-    let extracted =
-        value::crud::remove(&mut input.json_input, &path.segments).unwrap_or(Value::Null);
-
-    Ok(Output {
-        value: extracted,
-        trimmed_json: input.json_input,
-    })
-}
+const NAME: &str = "flow_run_info";
 
 fn build() -> BuildResult {
     Ok(
-        CmdBuilder::new(flow_lib::node_definition!("json_extract.json"))?
-            .check_name(JSON_EXTRACT)?
+        CmdBuilder::new(flow_lib::node_definition!("flow_run_info.json"))?
+            .check_name(NAME)?
             .build(run),
     )
 }
 
-flow_lib::submit!(CommandDescription::new(JSON_EXTRACT, |_| build()));
+flow_lib::submit!(CommandDescription::new(NAME, |_| build()));
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Deserialize, Debug)]
+struct Input {}
 
-    #[tokio::test]
-    async fn test_json_extract() {
-        let inputs = value::map! {
-            "json_input" => value::map! {
-                "a" => 1i64,
-                "b" => 2i64,
-                "c" => 3i64,
-            },
-            "field_path" => "c",
-        };
+#[derive(Serialize, Debug)]
+struct Output {
+    flow_owner: UserId,
+    started_by: UserId,
+}
 
-        let mut outputs = build().unwrap().run(<_>::default(), inputs).await.unwrap();
-        let value = outputs.remove("value").unwrap();
-        let trimmed = outputs.remove("trimmed_json").unwrap();
-        assert_eq!(value, Value::I64(3));
-        assert_eq!(
-            trimmed,
-            Value::Map(value::map! {
-                "a" => 1i64,
-                "b" => 2i64,
-            })
-        );
-    }
+async fn run(ctx: Context, _: Input) -> Result<Output, CommandError> {
+    Ok(Output {
+        flow_owner: ctx.flow_owner.id,
+        started_by: ctx.started_by.id,
+    })
 }
