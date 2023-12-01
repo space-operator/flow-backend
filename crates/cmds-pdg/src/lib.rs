55cebf350756debbb5c8d039f56efac34ce4b889
Duy Do <duydo@protonmail.com>
2023-12-01 16:07:50 +0700

    update_render_params (#27)

--- crates/cmds-pdg/src/lib.rs
+++ crates/cmds-pdg/src/lib.rs
@@ -1,8 +1,6 @@
-pub mod pdg_render;
-
-pub mod gen_pdg_attrs;
-pub mod parse_pdg_attrs;
-
 pub mod gen_metaplex_attrs;
-
+pub mod gen_pdg_attrs;
 pub mod get_effect_list;
+pub mod parse_pdg_attrs;
+pub mod pdg_render;
+pub mod update_render_params;
