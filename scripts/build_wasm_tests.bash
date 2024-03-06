7b39aa912c1fa3dca40e781b4017d935634db7eb
Duy Do <duydo@protonmail.com>
2024-03-06 18:50:32 +0700

    Test (#67)
    * less verbose
    
    * .

--- scripts/build_wasm_tests.bash
+++ scripts/build_wasm_tests.bash
@@ -1,10 +1,11 @@
-#!/usr/bin/bash
-set -Eeuxo pipefail
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 TARGET_DIR="$PWD/target/"
 
 for d in ./crates/space-wasm/tests/* ; do
     if [ -d "$d" ]; then
+        echo $d
         pushd "$d" > /dev/null
         if ! [ -d "target/" ] && [ "${1-}" = "sub" ]; then
             btrfs subvolume create target/
