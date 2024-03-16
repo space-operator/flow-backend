e372526ff4a1eaff82fa25cf505be0d00a6b0e0b
Duy Do <duydo@protonmail.com>
2024-03-16 08:30:08 +0700

    get_price_unchecked

--- crates/cmds-solana/src/pyth_price.rs
+++ crates/cmds-solana/src/pyth_price.rs
@@ -1,4 +1,3 @@
-use chrono::{DateTime, Utc};
 use flow_lib::{command::prelude::*, solana::Pubkey};
 use pyth_sdk_solana::state::SolanaPriceAccount;
 
@@ -36,13 +35,8 @@ async fn run(ctx: Context, input: Input) -> Result<Output, CommandError> {
         SolanaPriceAccount::account_to_feed(&input.price_feed_id, &mut sol_price_account)
             .map_err(|_| CommandError::msg("Invalid price feed account"))?;
 
-    let current_time = Utc::now()
-        .signed_duration_since(DateTime::UNIX_EPOCH)
-        .num_seconds();
-
-    let price = sol_price_feed
-        .get_price_no_older_than(current_time, 60)
-        .ok_or_else(|| CommandError::msg("price is None"))?;
+    let price = sol_price_feed.get_price_unchecked();
+    tracing::debug!("price: {:?}", price);
     Ok(Output { price: price.price })
 }
 
