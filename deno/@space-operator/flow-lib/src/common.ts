613be491c796de17254a21873ab715300d7f24e0
Duy Do <duydo@protonmail.com>
2024-03-27 19:11:36 +0700

    .

---
+++ deno/@space-operator/flow-lib/src/common.ts
@@ -0,0 +1,7 @@
+export type FlowId = number;
+export type FlowRunId = string;
+export type NodeId = string;
+export type UserId = string;
+export interface User {
+  id: UserId;
+}
---
+++ deno/@space-operator/flow-lib/src/deps.ts
@@ -0,0 +1,6 @@
+import * as bs58 from "@std/encoding/base58";
+import * as base64 from "@std/encoding/base64";
+
+import { Buffer } from "node:buffer";
+
+export { bs58, base64, Buffer };
