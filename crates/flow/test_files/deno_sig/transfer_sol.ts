38b9d5dd8b05c3d11fb26f45ce1927a39b4bd11d
Duy Do <duydo@protonmail.com>
2024-04-05 13:42:49 +0700

    Fix test

--- crates/flow/test_files/deno_sig/transfer_sol.ts
+++ crates/flow/test_files/deno_sig/transfer_sol.ts
@@ -1,5 +1,6 @@
 import * as lib from "jsr:@space-operator/flow-lib";
 import * as web3 from "npm:@solana/web3.js";
+import { encodeBase58 } from "jsr:@std/encoding@^0.220.1/base58";
 
 export default class TransferSol implements lib.CommandTrait {
   async run(
@@ -7,33 +8,34 @@ export default class TransferSol implements lib.CommandTrait {
     params: Record<string, any>
   ): Promise<Record<string, any>> {
     const fromPubkey = new web3.PublicKey(params.from);
-    let tx = new web3.Transaction()
-      .add(
-        web3.ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1000 })
-      )
-      .add(
+
+    // build the message
+    const message = new web3.TransactionMessage({
+      payerKey: fromPubkey,
+      recentBlockhash: (await ctx.solana.getLatestBlockhash()).blockhash,
+      instructions: [
+        web3.ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1000 }),
         web3.SystemProgram.transfer({
           fromPubkey,
           toPubkey: new web3.PublicKey(params.to),
           lamports: params.amount,
-        })
-      );
-    const { blockhash, lastValidBlockHeight } =
-      await lib.solana.getLatestBlockhash();
-    tx.recentBlockhash = blockhash;
-    tx.lastValidBlockHeight = lastValidBlockHeight;
-    tx.feePayer = fromPubkey;
+        }),
+      ],
+    }).compileToLegacyMessage();
 
+    // request signature from user
     const { signature, new_message } = await ctx.requestSignature(
       fromPubkey,
-      tx.serializeMessage()
+      message.serialize()
     );
-    if (new_message) {
-      tx = web3.Transaction.from(new_message);
-    }
-    tx.addSignature(fromPubkey, signature);
+
+    // submit
+    const tx = web3.Transaction.populate(
+      new_message ? web3.Message.from(new_message) : message,
+      [encodeBase58(signature)]
+    );
     return {
-      signature: new lib.Value(await ctx.solana.sendRawTransaction(tx.serialize())),
+      signature: await ctx.solana.sendRawTransaction(tx.serialize()),
     };
   }
 }
