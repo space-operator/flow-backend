1e82235dc6f18ee8df8574c1d86c3b5305f1eb9a
Duy Do <duydo@protonmail.com>
2024-04-15 19:28:50 +0700

    @space-operator/deno-command-rpc@0.9.4

--- deno/@space-operator/deno-command-rpc/src/mod.ts
+++ deno/@space-operator/deno-command-rpc/src/mod.ts
@@ -55,7 +55,7 @@ class PromiseSet {
   }
 
   async wait(): Promise<void> {
-    const promises = this.#promises;
+    const promises = [...this.#promises.values()];
     this.#promises = new Map();
     await Promise.allSettled(promises);
   }
