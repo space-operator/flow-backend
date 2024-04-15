e2e08da08d4124bc90955d0e3413505b7a84b120
Duy Do <duydo@protonmail.com>
2024-04-15 16:37:09 +0700

    @space-operator/deno-command-rpc@0.9.0

--- deno/@space-operator/deno-command-rpc/src/mod.ts
+++ deno/@space-operator/deno-command-rpc/src/mod.ts
@@ -1,4 +1,5 @@
 import {
+  type ServiceProxy,
   Context,
   type IValue,
   Value,
@@ -35,10 +36,142 @@ interface Response<T> {
   data: T;
 }
 
+type Console = typeof globalThis.console;
+type Level = "INFO" | "ERROR" | "WARN" | "DEBUG" | "TRACE";
+
+class CaptureLog implements Console {
+  #original: Console;
+  #service: ServiceProxy;
+  constructor(original: Console, service: ServiceProxy) {
+    this.#original = original;
+    this.#service = service;
+  }
+
+  #formatLogContent(data: any[]): string {
+    let str = "";
+    data.forEach((value, index) => {
+      if (index > 0) str += " ";
+      switch (typeof value) {
+        case "string":
+          str += value;
+          break;
+        case "number":
+          str += value.toString();
+          break;
+        case "bigint":
+          str += value.toString();
+          break;
+        case "boolean":
+          str += value.toString();
+          break;
+        case "symbol":
+          str += value.description;
+          break;
+        case "undefined":
+          str += "undefined";
+          break;
+        case "object":
+          str += JSON.stringify(value);
+          break;
+        case "function":
+          str += "[Function]";
+          break;
+      }
+    });
+    return str;
+  }
+
+  #call(level: Level, data: any[]) {
+    fetch(this.#service.base_url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        envelope: "",
+        svc_name: this.#service.name,
+        svc_id: this.#service.id,
+        input: {
+          level,
+          content: this.#formatLogContent(data),
+        },
+      }),
+    });
+  }
+
+  clear(): void {}
+  debug(...data: any[]): void {
+    this.#call("DEBUG", data);
+  }
+  error(...data: any[]): void {
+    this.#call("ERROR", data);
+  }
+  info(...data: any[]): void {
+    this.#call("INFO", data);
+  }
+  log(...data: any[]): void {
+    this.#call("INFO", data);
+  }
+  trace(...data: any[]): void {
+    this.#call("TRACE", data);
+  }
+  warn(...data: any[]): void {
+    this.#call("WARN", data);
+  }
+
+  assert(condition?: boolean | undefined, ...data: any[]): void {
+    return this.#original.assert(condition, ...data);
+  }
+
+  count(label?: string | undefined): void {
+    return this.#original.count(label);
+  }
+  countReset(label?: string | undefined): void {
+    return this.#original.countReset(label);
+  }
+  dir(item?: any, options?: any): void {
+    return this.#original.dir(item, options);
+  }
+  dirxml(...data: any[]): void {
+    return this.#original.dirxml(...data);
+  }
+  group(...data: any[]): void {
+    return this.#original.group(...data);
+  }
+  groupCollapsed(...data: any[]): void {
+    return this.#original.groupCollapsed(...data);
+  }
+  groupEnd(): void {
+    return this.#original.groupEnd();
+  }
+  profile(label?: string | undefined): void {
+    return this.#original.profile(label);
+  }
+  profileEnd(label?: string | undefined): void {
+    return this.#original.profileEnd(label);
+  }
+  table(tabularData?: any, properties?: string[] | undefined): void {
+    return this.#original.table(tabularData, properties);
+  }
+  time(label?: string | undefined): void {
+    return this.#original.time(label);
+  }
+  timeEnd(label?: string | undefined): void {
+    return this.#original.timeEnd(label);
+  }
+  timeLog(label?: string | undefined, ...data: any[]): void {
+    return this.#original.timeLog(label, ...data);
+  }
+  timeStamp(label?: string | undefined): void {
+    return this.#original.timeStamp(label);
+  }
+}
+
 export async function start(
   cmd: CommandTrait,
   listenOptions: Pick<ListenOptions, "hostname" | "port">
 ) {
+  const originalConsole = globalThis.console;
   const router = new Router();
   router.post("/call", async (ctx) => {
     const req: IRequest<any> = await ctx.request.body.json();
@@ -48,8 +181,16 @@ export async function start(
       const jsParams = params.toJSObject();
       let data: RunOutput;
       let success = false;
-      const context = new Context(input.ctx);
       try {
+        const context = new Context(input.ctx);
+        if (context.command?.log) {
+          globalThis.console = new CaptureLog(
+            originalConsole,
+            context.command?.log
+          );
+        } else {
+          globalThis.console = originalConsole;
+        }
         data = { Ok: new Value(await cmd.run(context, jsParams)).M! };
         success = true;
       } catch (error) {
