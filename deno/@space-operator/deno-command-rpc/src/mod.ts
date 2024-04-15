29121dc986226fc74ea5b558821f27fa00b5621a
Duy Do <duydo@protonmail.com>
2024-04-15 19:22:35 +0700

    @space-operator/deno-command-rpc@0.9.3

--- deno/@space-operator/deno-command-rpc/src/mod.ts
+++ deno/@space-operator/deno-command-rpc/src/mod.ts
@@ -39,16 +39,34 @@ interface Response<T> {
 type Console = typeof globalThis.console;
 type Level = "INFO" | "ERROR" | "WARN" | "DEBUG" | "TRACE";
 
+class PromiseSet {
+  #counter: bigint = 0n;
+  #promises: Map<bigint, Promise<void>> = new Map();
+
+  push(p: Promise<any>) {
+    const id = this.#counter;
+    this.#counter += 1n;
+    this.#promises.set(
+      id,
+      p.then(() => {
+        this.#promises.delete(id);
+      })
+    );
+  }
+
+  async wait(): Promise<void> {
+    const promises = this.#promises;
+    this.#promises = new Map();
+    await Promise.allSettled(promises);
+  }
+}
+
 class CaptureLog implements Console {
-  #original: Console;
+  #realConsole: Console;
   #service: ServiceProxy;
-  #promises: Promise<void>[];
-  constructor(
-    original: Console,
-    service: ServiceProxy,
-    promises: Promise<void>[]
-  ) {
-    this.#original = original;
+  #promises: PromiseSet;
+  constructor(original: Console, service: ServiceProxy, promises: PromiseSet) {
+    this.#realConsole = original;
     this.#service = service;
     this.#promises = promises;
   }
@@ -102,7 +120,7 @@ class CaptureLog implements Console {
           content: this.#formatLogContent(data),
         },
       }),
-    }).then(() => {});
+    });
     this.#promises.push(promise);
   }
 
@@ -127,50 +145,50 @@ class CaptureLog implements Console {
   }
 
   assert(condition?: boolean | undefined, ...data: any[]): void {
-    return this.#original.assert(condition, ...data);
+    return this.#realConsole.assert(condition, ...data);
   }
 
   count(label?: string | undefined): void {
-    return this.#original.count(label);
+    return this.#realConsole.count(label);
   }
   countReset(label?: string | undefined): void {
-    return this.#original.countReset(label);
+    return this.#realConsole.countReset(label);
   }
   dir(item?: any, options?: any): void {
-    return this.#original.dir(item, options);
+    return this.#realConsole.dir(item, options);
   }
   dirxml(...data: any[]): void {
-    return this.#original.dirxml(...data);
+    return this.#realConsole.dirxml(...data);
   }
   group(...data: any[]): void {
-    return this.#original.group(...data);
+    return this.#realConsole.group(...data);
   }
   groupCollapsed(...data: any[]): void {
-    return this.#original.groupCollapsed(...data);
+    return this.#realConsole.groupCollapsed(...data);
   }
   groupEnd(): void {
-    return this.#original.groupEnd();
+    return this.#realConsole.groupEnd();
   }
   profile(label?: string | undefined): void {
-    return this.#original.profile(label);
+    return this.#realConsole.profile(label);
   }
   profileEnd(label?: string | undefined): void {
-    return this.#original.profileEnd(label);
+    return this.#realConsole.profileEnd(label);
   }
   table(tabularData?: any, properties?: string[] | undefined): void {
-    return this.#original.table(tabularData, properties);
+    return this.#realConsole.table(tabularData, properties);
   }
   time(label?: string | undefined): void {
-    return this.#original.time(label);
+    return this.#realConsole.time(label);
   }
   timeEnd(label?: string | undefined): void {
-    return this.#original.timeEnd(label);
+    return this.#realConsole.timeEnd(label);
   }
   timeLog(label?: string | undefined, ...data: any[]): void {
-    return this.#original.timeLog(label, ...data);
+    return this.#realConsole.timeLog(label, ...data);
   }
   timeStamp(label?: string | undefined): void {
-    return this.#original.timeStamp(label);
+    return this.#realConsole.timeStamp(label);
   }
 }
 
@@ -178,27 +196,27 @@ export async function start(
   cmd: CommandTrait,
   listenOptions: Pick<ListenOptions, "hostname" | "port">
 ) {
-  const originalConsole = globalThis.console;
+  const realConsole = globalThis.console;
   const router = new Router();
   router.post("/call", async (ctx) => {
     const req: IRequest<any> = await ctx.request.body.json();
     if (req.svc_name === RUN_SVC) {
-      const input = req.input as RunInput;
+      const input: RunInput = req.input;
       const params = Value.fromJSON({ M: input.params });
       const jsParams = params.toJSObject();
       let data: RunOutput;
       let success = false;
-      const logPromises: Promise<void>[] = [];
+      const logPromises = new PromiseSet();
       try {
         const context = new Context(input.ctx);
         if (context.command?.log) {
           globalThis.console = new CaptureLog(
-            originalConsole,
+            realConsole,
             context.command?.log,
             logPromises
           );
         } else {
-          globalThis.console = originalConsole;
+          globalThis.console = realConsole;
         }
         data = { Ok: new Value(await cmd.run(context, jsParams)).M! };
         success = true;
@@ -206,7 +224,7 @@ export async function start(
         data = { Err: error.toString() };
         success = false;
       }
-      await Promise.allSettled(logPromises);
+      await logPromises.wait();
       const resp: Response<RunOutput> = {
         envelope: req.envelope,
         success,
