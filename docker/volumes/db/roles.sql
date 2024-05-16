f336d30007e1465a7951a6680ffad10138cae96e
Duy Do <duydo@protonmail.com>
2024-05-16 11:37:03 +0700

    Fix role

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -8,4 +8,4 @@ ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
 
-CREATE ROLE "flow_runner" WITH INHERIT NOCREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD :'flow_runner_password';
+CREATE ROLE "flow_runner" WITH INHERIT NOCREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS WITH PASSWORD :'flow_runner_password';
