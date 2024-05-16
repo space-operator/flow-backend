d8c65b92b9f83428bd17718e93b48c06840da9e6
Duy Do <duydo@protonmail.com>
2024-05-16 11:50:46 +0700

    fix

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -1,6 +1,6 @@
 -- NOTE: change to your own passwords for production environments
 \set pgpass `echo "$POSTGRES_PASSWORD"`
-\set flow_runner_password `echo "$FLOW_RUNNER_PASSWORD"`;
+\set flow_runner_password `echo "$FLOW_RUNNER_PASSWORD"`
 
 ALTER USER authenticator WITH PASSWORD :'pgpass';
 ALTER USER pgbouncer WITH PASSWORD :'pgpass';
@@ -8,4 +8,4 @@ ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
 
-CREATE ROLE "flow_runner" WITH INHERIT NOCREATEROLE NOCREATEDB LOGIN REPLICATION BYPASSRLS WITH PASSWORD :'flow_runner_password';
+CREATE ROLE "flow_runner" WITH INHERIT NOCREATEROLE NOCREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD :'flow_runner_password';
