ed9a8819717477fba65658ddcc683fee63bf7aee
Duy Do <duydo@protonmail.com>
2024-05-23 18:37:35 +0700

    .

--- docker/migrate-data.ts
+++ docker/migrate-data.ts
@@ -9,25 +9,31 @@ await load({ export: true });
 const SERVICE_ROLE_KEY = nonNull(Deno.env.get("SERVICE_ROLE_KEY"));
 const APIKEY = nonNull(Deno.env.get("APIKEY"));
 console.log("exporting data from https://dev-api.spaceoperator.com");
-const resp = await fetch("https://dev-api.spaceoperator.com/data/export", {
-  method: "POST",
-  headers: {
-    "accept-encoding": "br, gzip",
-    "x-api-key": APIKEY,
-  },
-});
-if (resp.status !== 200) {
-  console.log(await resp.text());
+const exportResp = await fetch(
+  "https://dev-api.spaceoperator.com/data/export",
+  {
+    method: "POST",
+    headers: {
+      "accept-encoding": "br, gzip",
+      "x-api-key": APIKEY,
+    },
+  }
+);
+if (exportResp.status !== 200) {
+  console.error(await exportResp.text());
   Deno.exit(1);
 }
-const data = await resp.json();
-Deno.exit(0);
+const data = await exportResp.json();
 console.log("importing data to http://localhost:8080");
-await fetch("http://localhost:8080/data/import", {
+const importResp = await fetch("http://localhost:8080/data/import", {
   headers: {
-    authorization: `BEARER ${SERVICE_ROLE_KEY}`,
+    authorization: `Bearer ${SERVICE_ROLE_KEY}`,
     "content-type": "application/json",
   },
   body: JSON.stringify(data),
   method: "POST",
 });
+if (importResp.status !== 200) {
+  console.error(await importResp.text());
+  Deno.exit(1);
+}
