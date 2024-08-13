cba89eb182b168aff5c9ae5fa0141e1d2545bc30
Duy Do <duydo@protonmail.com>
2024-08-13 13:57:43 +0700

    .

--- @space-operator/client/src/types/rest/start-flow-unverified.ts
+++ @space-operator/client/src/types/rest/start-flow-unverified.ts
@@ -1,10 +1,15 @@
-import { FlowRunId } from '../common.ts';
-import { IValue, Value } from '../../deps.ts';
+import { FlowRunId } from "../common.ts";
+import { IValue, Value } from "../../deps.ts";
+
+export interface SolanaActionConfig {
+  action_signer: string;
+  action_identity: string;
+}
 
 export interface StartFlowUnverifiedParams {
   inputs?: Record<string, IValue>;
   output_instructions?: boolean;
-  action_identity?: string;
+  action_config?: SolanaActionConfig;
 }
 
 export interface StartFlowUnverifiedOutput {
