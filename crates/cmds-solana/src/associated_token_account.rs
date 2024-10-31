a4fd7895da633b01e82e0bd34b80cba622535241
Duy Do <duydo@protonmail.com>
2024-10-31 13:26:04 +0700

    .

--- crates/cmds-solana/src/associated_token_account.rs
+++ crates/cmds-solana/src/associated_token_account.rs
@@ -24,17 +24,17 @@ flow_lib::submit!(CommandDescription::new(
 pub struct Input {
     #[serde_as(as = "AsPubkey")]
     owner: Pubkey,
-    #[serde(with = "value::keypair")]
-    fee_payer: Keypair,
-    #[serde(with = "value::pubkey")]
+    fee_payer: Wallet,
+    #[serde_as(as = "AsPubkey")]
     mint_account: Pubkey,
     #[serde(default = "value::default::bool_true")]
     submit: bool,
 }
 
+#[serde_as]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Output {
-    #[serde(default, with = "value::signature::opt")]
+    #[serde_as(as = "Option<AsSignature>")]
     signature: Option<Signature>,
 }
 
@@ -51,7 +51,7 @@ async fn run(mut ctx: Context, input: Input) -> Result<Output, CommandError> {
     let instructions = if input.submit {
         Instructions {
             fee_payer: input.fee_payer.pubkey(),
-            signers: [input.fee_payer.clone_keypair()].into(),
+            signers: [input.fee_payer].into(),
             instructions: [instruction].into(),
         }
     } else {
