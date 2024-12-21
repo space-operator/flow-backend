90bea4b32c13e8819125db3c8fa9f005f75c7c54
Duy Do <duydo@protonmail.com>
2024-12-21 16:34:19 +0700

    .

--- docker/supabase/migrations/20241214133549_flow_deployment.sql
+++ docker/supabase/migrations/20241214133549_flow_deployment.sql
@@ -1,26 +1,28 @@
 CREATE TABLE flow_deployments (
-    user_id UUID NOT NULL,
     id UUID NOT NULL,
+    user_id UUID NOT NULL,
     entrypoint INTEGER NOT NULL,
     start_permission JSONB NOT NULL,
     collect_instructions BOOL NOT NULL,
     action_identity TEXT NULL,
     action_config JSONB NULL,
-    fees JSONB [] NOT NULL
+    fees JSONB [] NOT NULL,
+    PRIMARY KEY (id)
 );
 
--- All wallets used in the deployment
+-- Wallet used in a deployment
 CREATE TABLE flow_deployments_wallets (
     user_id UUID NOT NULL,
     deployment_id UUID NOT NULL,
-    wallet_id BIGINT NOT NULL
+    wallet_id BIGINT NOT NULL,
+    PRIMARY KEY (deployment_id, wallet_id)
 );
 
--- All flows used in the deployment
+-- Flow used in a deployment
 CREATE TABLE flow_deployments_flows (
-    user_id UUID NOT NULL,
     deployment_id UUID NOT NULL,
     flow_id INTEGER NOT NULL,
+    user_id UUID NOT NULL,
     nodes JSONB [] NOT NULL,
     edges JSONB [] NOT NULL,
     environment JSONB NOT NULL,
@@ -28,7 +30,13 @@ CREATE TABLE flow_deployments_flows (
     instructions_bundling JSONB NOT NULL,
     is_public BOOLEAN NOT NULL,
     start_shared BOOLEAN NOT NULL,
-    start_unverified BOOLEAN NOT NULL
+    start_unverified BOOLEAN NOT NULL,
+    PRIMARY KEY (deployment_id, flow_id)
 );
 
-CREATE TABLE flow_deployments_tags (flow_id INTEGER, tag TEXT, deployment_id UUID);
+CREATE TABLE flow_deployments_tags (
+    flow_id INTEGER,
+    tag TEXT,
+    deployment_id UUID,
+    PRIMARY KEY (flow_id, tag, deployment_id)
+);
