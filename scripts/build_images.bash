fcd98fb90069f44552522f52996215607afb2a99
Duy Do <duydo@protonmail.com>
2025-01-23 08:32:35 +0700

    updates

--- scripts/build_images.bash
+++ scripts/build_images.bash
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eexuo pipefail
+
+CMD="podman"
+BUILD="podman build --pull=always"
+if [[ "${1:-}" == docker ]]; then
+    CMD="docker"
+    BUILD="docker build --pull"
+fi
+
+echo Using $CMD
+
+PROFILE=${PROFILE:-release}
 
 NAME="space-operator/flow-server"
 DOCKERFILE="crates/flow-server/Dockerfile"
@@ -10,17 +21,17 @@ if [[ "$(git describe --always --dirty)" == *-dirty ]]; then
 fi
 
 set -x
-time docker build --pull=always --target rustc -t "$NAME-rustc:latest" -f "$DOCKERFILE" .
-time docker build --pull=always --target planner -t "$NAME-planner:latest" -f "$DOCKERFILE" .
-time docker build --pull=always --target cacher -t "$NAME-cacher:latest" -f "$DOCKERFILE" .
+time $BUILD --target rustc -t "$NAME-rustc:latest" -f "$DOCKERFILE" .
+time $BUILD --target planner -t "$NAME-planner:latest" -f "$DOCKERFILE" .
+time $BUILD --target cacher --build-arg PROFILE=$PROFILE -t "$NAME-cacher:$PROFILE-latest" -f "$DOCKERFILE" .
 
 BUILDER_TAG=$RANDOM
-time docker build --pull=always --target builder -t "$NAME-builder:$BUILDER_TAG" -f "$DOCKERFILE" .
+time $BUILD --target builder --build-arg PROFILE=$PROFILE -t "$NAME-builder:$BUILDER_TAG" -f "$DOCKERFILE" .
 
 COMMIT="$(git rev-parse --verify HEAD)$DIRTY"
-time docker build --pull=always -t "$NAME:$COMMIT" -f "$DOCKERFILE" .
+time $BUILD -t "$NAME:$COMMIT" -f "$DOCKERFILE" .
 
 BRANCH="${BRANCH:-$(git rev-parse --abbrev-ref HEAD)$DIRTY}"
-docker tag $NAME:$COMMIT $NAME:$BRANCH
+$CMD tag $NAME:$COMMIT $NAME:$BRANCH
 
-docker image rm "$NAME-builder:$BUILDER_TAG"
+$CMD image rm "$NAME-builder:$BUILDER_TAG"
