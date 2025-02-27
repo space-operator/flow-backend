f66d68006863e0c9ee07a36fc466314429ad0e23
Duy Do <duydo@protonmail.com>
2025-02-27 13:35:21 +0700

    Update deps

--- @space-operator/client/src/deps.ts
+++ @space-operator/client/src/deps.ts
@@ -1,5 +1,5 @@
 export * as web3 from "npm:@solana/web3.js@1";
-export * as lib from "jsr:@space-operator/flow-lib@0.10.0";
+export * as lib from "jsr:@space-operator/flow-lib@0.11.0";
 export * as bs58 from "jsr:@std/encoding@^0.221.0/base58";
-export { Value, type IValue } from "jsr:@space-operator/flow-lib@0.10.0/value";
+export { Value, type IValue } from "jsr:@space-operator/flow-lib@0.11.0/value";
 export type { Session as SupabaseSession } from "npm:@supabase/auth-js@2";
