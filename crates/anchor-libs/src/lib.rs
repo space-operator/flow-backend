3e6436b75fa6a715bc3a76c86e57ec3a1199430f
Duy Do <duydo@protonmail.com>
2025-03-11 13:34:51 +0700

    .

--- crates/anchor-libs/src/lib.rs
+++ crates/anchor-libs/src/lib.rs
@@ -3,3 +3,19 @@
 #[doc = " Generated external program declaration of program `spl_account_compression`."]
 #[doc = ""]
 pub mod spl_account_compression;
+
+#[doc = " Generated external program declaration of program `candy_guard`."]
+#[doc = ""]
+pub mod candy_guard;
+
+#[doc = " Generated external program declaration of program `candy_machine_core`."]
+#[doc = ""]
+pub mod candy_machine_core;
+
+#[doc = " Generated external program declaration of program `candy_guard`."]
+#[doc = ""]
+pub mod mpl_core_candy_guard;
+
+#[doc = " Generated external program declaration of program `candy_machine_core`."]
+#[doc = ""]
+pub mod mpl_core_candy_machine_core;
