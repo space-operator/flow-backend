fa7d49a67203f61b7a478abf2b500d43e2437f49
Duy Do <duydo@protonmail.com>
2025-03-19 20:24:22 +0700

    .

--- crates/anchor-libs/src/lib.rs
+++ crates/anchor-libs/src/lib.rs
@@ -11,11 +11,3 @@ pub mod candy_guard;
 #[doc = " Generated external program declaration of program `candy_machine_core`."]
 #[doc = ""]
 pub mod candy_machine_core;
-
-#[doc = " Generated external program declaration of program `candy_guard`."]
-#[doc = ""]
-pub mod mpl_core_candy_guard;
-
-#[doc = " Generated external program declaration of program `candy_machine_core`."]
-#[doc = ""]
-pub mod mpl_core_candy_machine_core;
