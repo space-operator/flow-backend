2a279691f039bf56ce1a81c9f43e8df0abb91184
Duy Do <duydo@protonmail.com>
2025-04-14 21:41:52 +0700

    .

--- crates/command-rpc/src/lib.rs
+++ crates/command-rpc/src/lib.rs
@@ -1,8 +1,11 @@
 //! RPC specification for calling a command on a remote node
 
-pub mod client;
-pub mod server;
+// pub mod client;
+// pub mod server;
 
 pub mod command_capnp {
     include!(concat!(env!("OUT_DIR"), "/command_capnp.rs"));
 }
+
+pub mod flow_side;
+pub mod command_side;
