20ac51a0d8c19841825885b1801be8cde56e40ef
Duy Do <duydo@protonmail.com>
2025-06-18 14:51:37 +0700

    .

--- crates/cmds-std-server/src/main.rs
+++ crates/cmds-std-server/src/main.rs
@@ -1,8 +1,12 @@
+use command_rpc::command_side::command_server::Config;
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let mut data = std::fs::read(std::env::args().nth(1).unwrap()).unwrap();
+    let config: Config = simd_json::from_slice(&mut data).unwrap();
     let local = tokio::task::LocalSet::new();
     local
-        .run_until(command_rpc::command_side::command_server::serve(todo!()))
+        .run_until(command_rpc::command_side::command_server::serve(config))
         .await
         .unwrap();
 }
--- crates/command-rpc/src/command_side/command_server.rs
+++ crates/command-rpc/src/command_side/command_server.rs
@@ -1,26 +1,41 @@
-use std::{collections::BTreeSet, net::SocketAddr};
-
 use anyhow::Context;
 use flow_lib::command::collect_commands;
 use iroh::{Endpoint, NodeAddr};
+use serde::Deserialize;
+use std::{collections::BTreeSet, net::SocketAddr};
 use url::Url;
 
 use crate::flow_side::address_book::{self, AddressBookExt};
 
 use super::command_factory::{self, CommandFactoryExt};
 
+#[derive(Deserialize)]
 pub struct Config {
-    pub flow_server: Vec<FlowServerAddress>,
+    pub flow_server_url: Url,
     pub secret_key: iroh::SecretKey,
 }
 
+#[derive(Deserialize)]
 pub struct FlowServerAddress {
     pub node_id: iroh::PublicKey,
     pub direct_addresses: BTreeSet<SocketAddr>,
     pub relay_url: Url,
 }
 
+#[derive(Deserialize)]
+struct InfoResponse {
+    iroh: FlowServerAddress,
+}
+
 pub async fn serve(config: Config) -> Result<(), anyhow::Error> {
+    let info_url = config.flow_server_url.join("/info")?;
+    let server = reqwest::get(info_url)
+        .await?
+        .json::<InfoResponse>()
+        .await?
+        .iroh;
+    let servers = [server];
+
     let commands = collect_commands();
     let endpoint = Endpoint::builder()
         .secret_key(config.secret_key)
@@ -38,7 +53,7 @@ pub async fn serve(config: Config) -> Result<(), anyhow::Error> {
     let relay_url: Url = endpoint.home_relay().initialized().await?.into();
     let availables: Vec<String> = commands.keys().map(|name| name.to_string()).collect();
     let mut clients = Vec::new();
-    for addr in &config.flow_server {
+    for addr in &servers {
         let client = address_book::connect_iroh(
             endpoint.clone(),
             NodeAddr {
