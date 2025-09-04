982a639ec7978508e9914185539ca2635eba1b8a
Duy Do <duydo@protonmail.com>
2025-09-04 15:48:50 +0700

    less buckets

--- crates/flow-server/src/main.rs
+++ crates/flow-server/src/main.rs
@@ -213,7 +213,7 @@ async fn main() {
         let dashboard_input = DashboardInput {
             buckets_for_metrics: vec![(
                 Matcher::Full("event_lag".to_owned()),
-                &[0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0],
+                &[0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0],
             )],
         };
         let metrics_scope = create_metrics_actx_scope(&dashboard_input).unwrap();
