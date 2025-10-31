3b3c2945bfb0193898ca4a8a3177f2219a0a1127
Duy Do <duydo@protonmail.com>
2025-10-31 14:37:21 +0700

    .

--- lib/x402-actix/src/lib.rs
+++ lib/x402-actix/src/lib.rs
@@ -1,5 +1,23 @@
+use x402_rs::{network::Network, types::PaymentRequirements};
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+fn create_exact_paypemt_requirements(network: Network) -> PaymentRequirements {
+    PaymentRequirements {
+        scheme: x402_rs::types::Scheme::Exact,
+        network,
+        max_amount_required: (),
+        resource: (),
+        description: (),
+        mime_type: (),
+        output_schema: (),
+        pay_to: (),
+        max_timeout_seconds: (),
+        asset: (),
+        extra: (),
+    }
 }
 
 #[cfg(test)]
