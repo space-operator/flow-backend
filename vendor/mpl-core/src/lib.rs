2810e7c93fb8a0cedaff118e6d71b9c2a4e82934
Duy Do <duydo@protonmail.com>
2025-11-06 17:08:57 +0700

    .

--- vendor/mpl-core/src/lib.rs
+++ vendor/mpl-core/src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(warnings)]
 
+pub mod remainder_vec;
 mod generated;
 mod hooked;
 mod indexable_asset;
@@ -10,3 +11,16 @@ pub use hooked::*;
 pub use indexable_asset::*;
 
 impl Copy for generated::types::Key {}
+
+pub trait TryToVec {
+    fn try_to_vec(&self) -> Result<Vec<u8>, borsh::io::Error>;
+}
+
+impl<T> TryToVec for T
+where
+    T: borsh::BorshSerialize,
+{
+    fn try_to_vec(&self) -> Result<Vec<u8>, borsh::io::Error> {
+        borsh::to_vec(self)
+    }
+}
