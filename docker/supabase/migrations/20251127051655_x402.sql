b531764782f842e6d1580077a41604584bbe24c8
Duy Do <duydo@protonmail.com>
2025-11-28 11:15:37 +0700

    table

--- docker/supabase/migrations/20251127051655_x402.sql
+++ docker/supabase/migrations/20251127051655_x402.sql
@@ -2,6 +2,16 @@ create type x402network as enum (
     'base', 'base-sepolia',
     'solana', 'solana-devnet'
 );
+
+create table flow_x402_fees (
+    id bigserial primary key,
+    deployment_id uuid not null references flow_deployments(id),
+    network x402network not null,
+    pay_to bigint not null references wallets(id),
+    amount decimal not null
+);
+alter table flow_x402_fees enable row level security;
+
 create table flow_deployments_x402_fees (
     id bigserial primary key,
     deployment_id uuid not null references flow_deployments(id),
@@ -9,3 +19,4 @@ create table flow_deployments_x402_fees (
     pay_to bigint not null references wallets(id),
     amount decimal not null
 );
+alter table flow_deployments_x402_fees enable row level security;
