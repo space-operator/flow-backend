22abbbb875e85f81680146aa6cea1dcf841d6746
Duy Do <duydo@protonmail.com>
2025-12-03 21:14:10 +0700

    update

--- crates/flow-server/src/main.rs
+++ crates/flow-server/src/main.rs
@@ -27,6 +27,7 @@ use metrics_rs_dashboard_actix::{
 use std::{convert::Infallible, sync::Arc, time::Duration};
 use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
 use utils::address_book::AddressBook;
+use x402_actix::{facilitator_client::FacilitatorClient, middleware::X402Middleware};
 
 // avoid commands being optimized out by the compiler
 #[cfg(feature = "commands")]
@@ -127,6 +128,10 @@ async fn main() {
         }
     }
     */
+
+    let facilitator =
+        FacilitatorClient::try_new("https://www.x402.org/facilitator/".parse().unwrap()).unwrap();
+    let x402 = X402Middleware::new(facilitator).await.unwrap();
 
     let store = RequestStore::new_app_data();
 
