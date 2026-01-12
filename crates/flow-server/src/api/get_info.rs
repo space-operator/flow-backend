e2d0e776fcb98a97d87482d7c0259a072271b05c
Duy Do <duydo@protonmail.com>
2026-01-12 19:13:01 +0700

    host

--- crates/flow-server/src/api/get_info.rs
+++ crates/flow-server/src/api/get_info.rs
@@ -1,30 +1,33 @@
 use super::prelude::*;
 use crate::db_worker::{GetIrohInfo, IrohInfo};
 use actix::SystemService;
-use actix_web::{HttpResponseBuilder, http::header::ContentType};
+use actix_web::{HttpResponseBuilder, dev::ConnectionInfo, http::header::ContentType};
 use url::Url;
 
 #[derive(Serialize)]
-struct Output {
+struct Output<'a> {
     supabase_url: Url,
     anon_key: String,
     iroh: IrohInfo,
+    host: &'a str,
 }
 
 pub fn service(config: &Config) -> impl HttpServiceFactory + 'static {
     let supabase_url = config.supabase.endpoint.url.clone();
     let anon_key = config.supabase.anon_key.clone();
-    web::resource("/info").route(web::get().to(move || {
+    web::resource("/info").route(web::get().to(move |info: ConnectionInfo| {
         let supabase_url = supabase_url.clone();
         let anon_key = anon_key.clone();
 
         async move {
             let db_worker = DBWorker::from_registry();
             let iroh = db_worker.send(GetIrohInfo).await.unwrap().unwrap();
+            let host = info.host();
             let output = Output {
                 supabase_url,
                 anon_key,
                 iroh,
+                host,
             };
             let json = simd_json::to_vec(&output).unwrap();
             HttpResponseBuilder::new(StatusCode::OK)
