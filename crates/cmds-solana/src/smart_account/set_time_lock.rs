ddd1d031c94749b131eca4d55e5654ec876b2c81
enzotar <enzotar3000@gmail.com>
2026-04-06 13:45:20 -0700

    smart account updates

--- crates/cmds-solana/src/smart_account/set_time_lock.rs
+++ crates/cmds-solana/src/smart_account/set_time_lock.rs
@@ -22,7 +22,7 @@ pub struct Input {
     pub fee_payer: Wallet,
     #[serde_as(as = "AsPubkey")]
     pub settings: Pubkey,
-    pub settings_authority: Wallet,
+    pub signer: Wallet,
     pub time_lock: u32,
     #[serde(default)]
     pub memo: Option<String>,
@@ -40,7 +40,7 @@ pub struct Output {
 async fn run(mut ctx: CommandContext, input: Input) -> Result<Output, CommandError> {
     let accounts = vec![
         AccountMeta::new(input.settings, false),
-        AccountMeta::new_readonly(input.settings_authority.pubkey(), true),
+        AccountMeta::new_readonly(input.signer.pubkey(), true),
         AccountMeta::new(input.fee_payer.pubkey(), true),
         AccountMeta::new_readonly(solana_system_interface::program::ID, false),
         AccountMeta::new_readonly(PROGRAM_ID, false),
@@ -62,7 +62,7 @@ async fn run(mut ctx: CommandContext, input: Input) -> Result<Output, CommandErr
     let ins = Instructions {
         lookup_tables: None,
         fee_payer: input.fee_payer.pubkey(),
-        signers: [input.fee_payer.clone(), input.settings_authority.clone()]
+        signers: [input.fee_payer.clone(), input.signer.clone()]
             .into_iter()
             .collect(),
         instructions: vec![instruction],
